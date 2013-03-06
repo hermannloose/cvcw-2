21c3c90b8054a4050286447a0ff28e2091675cad
Hermann Loose <hermannloose@gmail.com>
2013-03-06 13:41:26 +0100

    Fix calculation of neighbouring pixels to actually include all 8 candidates.

--- PixelImage.cpp
+++ PixelImage.cpp
@@ -27,12 +27,12 @@ namespace mser {
     PixelVector *neighbours = new PixelVector();
     neighbours->reserve(8);
 
-
     Pixel *neighbour = NULL;
 
     if (pixel->x > 0) {
       PixelVector *leftColumn = columns->at(pixel->x - 1);
 
+      // TOP LEFT
       if (pixel->y > 0) {
         neighbour = leftColumn->at(pixel->y - 1);
         if (neighbour != NULL) {
@@ -41,12 +41,14 @@ namespace mser {
         neighbour = NULL;
       }
 
+      // LEFT
       neighbour = leftColumn->at(pixel->y);
       if (neighbour != NULL) {
         neighbours->append(neighbour);
       }
       neighbour = NULL;
 
+      // BOTTOM LEFT
       if (pixel->y < height - 1) {
         neighbour = leftColumn->at(pixel->y + 1);
         if (neighbour != NULL) {
@@ -56,9 +58,30 @@ namespace mser {
       }
     }
 
+    PixelVector *currentColumn = columns->at(pixel->x);
+
+    // TOP
+    if (pixel->y > 0) {
+      neighbour = currentColumn->at(pixel->y - 1);
+      if (neighbour != NULL) {
+        neighbours->append(neighbour);
+      }
+      neighbour = NULL;
+    }
+
+    // BOTTOM
+    if (pixel->y < height - 1) {
+      neighbour = currentColumn->at(pixel->y + 1);
+      if (neighbour != NULL) {
+        neighbours->append(neighbour);
+      }
+      neighbour = NULL;
+    }
+
     if (pixel->x < width - 1) {
       PixelVector *rightColumn = columns->at(pixel->x + 1);
 
+      // TOP RIGHT
       if (pixel->y > 0) {
         neighbour = rightColumn->at(pixel->y - 1);
         if (neighbour != NULL) {
@@ -67,12 +90,14 @@ namespace mser {
         neighbour = NULL;
       }
 
+      // RIGHT
       neighbour = rightColumn->at(pixel->y);
       if (neighbour != NULL) {
         neighbours->append(neighbour);
       }
       neighbour = NULL;
 
+      // BOTTOM RIGHT
       if (pixel->y < height - 1) {
         neighbour = rightColumn->at(pixel->y + 1);
         if (neighbour != NULL) {
